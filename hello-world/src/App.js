11fa824a9d72bd932ea012e29e59a4af01c38a07
Manjula Rathnayaka <manjula.cse@gmail.com>
2023-11-28 14:28:17 +0530

    add login

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => {window.location.href="/auth/login"}}>Login</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
